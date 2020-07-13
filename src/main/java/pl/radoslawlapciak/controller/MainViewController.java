78cdbf129b89970b32adf969bb1b0361412f9d69
216826 Radosław Łapciak <216826@edu.p.lodz.pl>
2020-07-13 14:07:57 +0200

    Add implementation of handleLoadImageButtonAction method.

--- src/main/java/pl/radoslawlapciak/controller/MainViewController.java
+++ src/main/java/pl/radoslawlapciak/controller/MainViewController.java
@@ -2,13 +2,35 @@ package pl.radoslawlapciak.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class MainViewController {
 
     @FXML
+    private GridPane imageGrid;
+
+    @FXML
     private void handleLoadImageButtonAction(ActionEvent event){
-        event.consume();
-        System.out.println("Hello, World!");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image files", ".bmp", "*.png", "*.jpg", "*.gif"));
+        fileChooser.setTitle("select an image");
+        File file = fileChooser.showOpenDialog(new Stage());
+        if(file != null){
+            for(Node node : imageGrid.getChildren()){
+                if(node instanceof ImageView){
+                    ImageView imageView = (ImageView) node;
+                    Image image = new Image(file.toURI().toString());
+                    imageView.setImage(image);
+                }
+            }
+        }
     }
 
 }
