54ded569adc66b073287686d7998953243424e24
216826 Radosław Łapciak <216826@edu.p.lodz.pl>
2020-07-13 20:36:42 +0200

    point is added after click

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # recruitment-task
 ### Additional functions
 * load an image
+### Optional functions
+* determining the coordinates of a point relative to the image
--- src/main/java/pl/radoslawlapciak/controller/MainViewController.java
+++ src/main/java/pl/radoslawlapciak/controller/MainViewController.java
@@ -5,7 +5,12 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -16,11 +21,14 @@ import java.util.List;
 public class MainViewController {
 
     @FXML
+    private AnchorPane imagesPane;
+
+    @FXML
     private GridPane imageGrid;
 
     @FXML
     private void handleLoadImageButtonAction(ActionEvent event) {
-        File file = showAndGetFileFromFileChooser("Select an image", "Image files",".bmp", "*.png", "*.jpg", "*.gif" );
+        File file = showAndGetFileFromFileChooser("Select an image", "Image files", ".bmp", "*.png", "*.jpg", "*.gif");
         if (file != null) {
             Image image = new Image(file.toURI().toString());
             for (ImageView imageView : getAllImageViewsFromGridPane()) {
@@ -29,7 +37,20 @@ public class MainViewController {
         }
     }
 
-    private File showAndGetFileFromFileChooser(String title, String description, String ... extensions){
+    @FXML
+    private void handleImageClick(MouseEvent event) {
+        event.consume();
+        for (Node node : imageGrid.getChildren()) {
+            if (node instanceof AnchorPane) {
+                Circle circle = new Circle(event.getX() + 5, event.getY() + 5, 5);
+                circle.setFill(Color.RED);
+                AnchorPane anchorPane = (AnchorPane) node;
+                anchorPane.getChildren().add(circle);
+            }
+        }
+    }
+
+    private File showAndGetFileFromFileChooser(String title, String description, String... extensions) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter(description, extensions));
         fileChooser.setTitle(title);
@@ -39,8 +60,12 @@ public class MainViewController {
     private List<ImageView> getAllImageViewsFromGridPane() {
         List<ImageView> imageViews = new ArrayList<>();
         for (Node node : imageGrid.getChildren()) {
-            if(node instanceof ImageView) {
-                imageViews.add((ImageView) node);
+            if (node instanceof AnchorPane) {
+                for (Node imageView : ((AnchorPane) node).getChildren()) {
+                    if (imageView instanceof ImageView) {
+                        imageViews.add((ImageView) imageView);
+                    }
+                }
             }
         }
         return imageViews;
